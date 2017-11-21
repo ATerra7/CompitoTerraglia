e224cdd2b3d13cd9c6bc09794ac20424690f7c89
ATerra7 <marco.terra99@gmail.com>
2017-11-21 21:33:51 +0100

    Primo commit dal branch master

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* Terraglia Marco */
+
+/*Riga del conflitto */
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Ecco il ciclo da 1 a 10: "<<endl;
+
+	for(int i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+
+	System("pause");
+}
